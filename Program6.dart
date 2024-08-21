bb1ee0e3253084b0c2a86f24810f0d22c641d240
Prasad Ghodake <prasadghodake2803@gmail.com>
2024-08-21 22:29:05 +0530

    Update Program6.dart

--- Program6.dart
+++ Program6.dart
@@ -1,7 +1,7 @@
 import "dart:io";
 
 void main() {
-  int row = 5;
+   int row = int.parse(stdin.readLineSync()!);
   int x = 14;
   for (int i = row; i >= 1; i--) {
     for (int j = row; j >= 1; j--) {
